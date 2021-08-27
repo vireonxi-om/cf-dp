e0e6786c90ef0f81405c8f579fbf5f4581c7f3d7
rheallyc <vireonxi@gmail.com>
2021-08-27 14:53:11 +0000

    Solve 1501B: Napoleon Cake

---
+++ solutions/1501B_Napoleon_Cake.cpp
@@ -0,0 +1,36 @@
+// Problem: Napoleon Cake
+// URL: https://codeforces.com/problemset/problem/1501/B
+// Rating: 900
+// Tags: dp, implementation, sortings
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long  ll;
+int n, ans[210000];
+
+void solve(){
+    for(int i=0; i<=n; i++) ans[i]=0;
+    scanf("%d", &n);
+    int num;
+    for(int i=1; i<=n; i++){
+        scanf("%d", &num);
+        int t=min(i, num);
+        ans[i-t+1]=max(ans[i-t+1], t);
+    }
+    int now=0;
+    for(int i=1; i<=n; i++){
+        int t=max(now, ans[i]);
+        if(t){ ans[i]=1; t--;}
+        else ans[i]=0;
+        now=t;
+    }
+    for(int i=1; i<=n; i++) printf("%d ", ans[i]);
+    printf("\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
