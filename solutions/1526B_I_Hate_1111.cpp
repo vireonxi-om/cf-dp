b31662ca3ae9cd638813fc523e09b0795a883a79
rheallyc <vireonxi@gmail.com>
2021-09-09 22:31:16 +0000

    Solve 1526B: I Hate 1111

---
+++ solutions/1526B_I_Hate_1111.cpp
@@ -0,0 +1,32 @@
+// Problem: I Hate 1111
+// URL: https://codeforces.com/problemset/problem/1526/B
+// Rating: 1400
+// Tags: dp, math, number theory
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+
+void solve(){
+    scanf("%d", &n);
+    bool can=false;
+    for(int i=0; i<=110; i++){
+        if(n<i*11) break;
+        if((n-i*11)%111==0){
+            can=true;
+            break;
+        }
+    }
+    if(can) printf("YES\n");
+    else printf("NO\n");
+}
+
+int main(){
+    int t;
+    scanf("%d", &t);
+    while(t--) solve();
+    return 0;
+}
+
+
