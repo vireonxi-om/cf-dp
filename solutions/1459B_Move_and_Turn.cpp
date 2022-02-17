335e1a0375db9cfff6e6b76d33d94dcd84a8bad6
rheallyc <vireonxi@gmail.com>
2022-02-17 03:10:48 +0000

    Solve 1459B: Move and Turn

---
+++ solutions/1459B_Move_and_Turn.cpp
@@ -0,0 +1,18 @@
+// Problem: Move and Turn
+// URL: https://codeforces.com/problemset/problem/1459/B
+// Rating: 1300
+// Tags: dp, math
+// Language: C++17 (GCC 7-32)
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+int n;
+
+int main(){
+    scanf("%d", &n);
+    int k=n/2, result;
+    if(n&1) result=2*(k+1)*(k+2);
+    else result=(k+1)*(k+1);
+    printf("%d\n", result);
+    return 0;
+}
